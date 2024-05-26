16c57ffbd213f25b01f840088cbef20677c1f181
shufolislam1 <shufolislam1255@gmail.com>
2024-05-26 22:45:55 +0600

    right bottom part

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     var fullImgCard = document.querySelector('.full-img-card');
     var afterClick = document.querySelector('.after-click');
-    // var beforeClick = document.querySelector('.before-click');
     var h1 = document.querySelector('.text');
     var triangle = document.querySelector('.triangle')
 
     fullImgCard.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent the default link behavior
+        event.preventDefault();
         afterClick.style.display = 'block';
-        // beforeClick.style.display = 'none';
         h1.style.display = 'none';
         triangle.style.display = 'none';
     });
